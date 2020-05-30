e4a248d611dd355eae31b0a9d54b406c9b3fac80
hitalo07 <hitalocunhadesousa@gmail.com>
2020-05-30 18:13:47 -0300

    chore: Define Color

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -9,7 +9,7 @@ export default {
   primary: {
     contrastText: white,
     dark: colors.indigo[900],
-    main: "#4CFA50",
+    main: "#409944",
     light: colors.indigo[100]
   },
   secondary: {
